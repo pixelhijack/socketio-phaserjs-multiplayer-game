71747ed74f197d6df0927d002a40f4df8a4d7339
pixelhijack <pothattila@gmail.com>
2016-08-21 10:17:34 +0000

    Phaser, sprites, texture atlas, animations. first steps

--- client/index.js
+++ client/index.js
@@ -1,8 +1,42 @@
 var socket = require('./src/socket.js');
 
-window.onload = function(){
+function Play(){
+  
+  var frogman, 
+      minotaur;
+  
+  this.preload = function(){
+    console.log('[Phaser] preload');
+    this.game.load.atlas('characters', './assets/spritesheet.png', './assets/sprites.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+  };
+  
+  this.create = function(){
+    console.log('[Phaser] create');
+    this.game.stage.backgroundColor = "#FFFFFF";
+    frogman = this.game.add.sprite(40, 100, 'characters');
+    frogman.animations.add('walk', ['02', '04', '05', '07', '09', '10'], 10, true);
+    frogman.animations.add('attack', ['01', '03', '06', '08'], 10, true);
+    frogman.animations.play('attack');
     
-    console.log('phaser', Phaser);
+    minotaur = this.game.add.sprite(40, 150, 'characters');
+    minotaur.animations.add('walk', ['18', '22', '25', '26'], 10, true);
+    minotaur.animations.play('walk');
+  };
+  
+  this.update = function(){
+    console.log('[Phaser] update');
+  };
+}
+
+window.onload = function(){
+  
+    var game = new Phaser.Game(
+      window.innerWidth, 
+      window.innerHeight, 
+      Phaser.AUTO);
+      
+    game.state.add('Play', Play);
+    game.state.start('Play');
     
     socket.on('connect', function () {
       console.log('[CLIENT] on:connect', arguments);
