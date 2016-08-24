2214b92e6bfc2ccdd11addd68d7a950a0d866d1c
pixelhijack <pothattila@gmail.com>
2016-08-24 14:17:39 +0000

    PHASER: extracting out character classes

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -7937,6 +7937,51 @@
   \*****************************************/
 /***/ function(module, exports) {
 
+	/*
+	*   @class Creature
+	*   @extends Phaser.Sprite
+	*/
+	function Creature(game, x, y, sprite){
+	  this.game = game;
+	  Phaser.Sprite.call(this, game, x, y, sprite);
+	  this.game.add.existing(this);
+	  this.game.physics.enable(this, Phaser.Physics.ARCADE);
+	  this.anchor.setTo(0.5, 0.5);
+	}
+	Creature.prototype = Object.create(Phaser.Sprite.prototype);
+	Creature.prototype.constructor = Creature;
+	/*
+	*   @class Frogman
+	*   @extends Creature
+	*/
+	function Frogman(game, x, y, sprite){
+	  Creature.call(this, game, x, y, sprite);
+	  this.animations.add('walk', ['02', '04', '05', '07', '09', '10'], 10, true);
+	  this.animations.add('attack', ['01', '03', '06', '08'], 10, true);
+	  this.scale.x = -1;
+	}
+	Frogman.prototype = Object.create(Creature.prototype);
+	Frogman.prototype.constructor = Frogman;
+	
+	/*
+	*   @class Minotaur
+	*   @extends Creature
+	*/
+	function Minotaur(game, x, y, sprite){
+	  Creature.call(this, game, x, y, sprite);
+	  this.anchor.setTo(0.5, 0);
+	  
+	  this.animations.add('stand', ['22'], 10, true);
+	  this.animations.add('walk', ['18', '22', '25', '26'], 10, true);
+	  this.animations.add('attack', ['16','20','24','27','28', '29'], 12, true);
+	}
+	Minotaur.prototype = Object.create(Creature.prototype);
+	Minotaur.prototype.constructor = Minotaur;
+	
+	/*
+	*   FrogmanVsMinotaur
+	*   @Phaser gamestate
+	*/
 	function FrogmanVsMinotaur(){
 	  
 	  var frogman, 
@@ -7960,19 +8005,9 @@
 	    this.game.stage.backgroundColor = "#FFFFFF";
 	    this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'background');
 	    
-	    frogman = this.game.add.sprite(350, 245, 'characters');
-	    frogman.animations.add('walk', ['02', '04', '05', '07', '09', '10'], 10, true);
-	    frogman.animations.add('attack', ['01', '03', '06', '08'], 10, true);
-	    frogman.scale.x = -1;
-	    frogman.anchor.setTo(0.5, 0.5);
+	    frogman = new Frogman(this.game, 350, 245, 'characters');
 	    
-	    minotaur = this.game.add.sprite(40, 215, 'characters');
-	    minotaur.animations.add('stand', ['22'], 10, true);
-	    minotaur.animations.add('walk', ['18', '22', '25', '26'], 10, true);
-	    minotaur.animations.add('attack', ['16','20','24','27','28', '29'], 12, true);
-	    frogman.anchor.setTo(0.5, 0.5);
-	    
-	    this.game.physics.enable(minotaur, Phaser.Physics.ARCADE);
+	    minotaur = new Minotaur(this.game, 40, 215, 'characters');
 	    
 	    keys = this.game.input.keyboard.createCursorKeys();
 	    keys.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
